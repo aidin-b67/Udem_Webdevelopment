50dc89548ba3178a66c0fc85c1b6e628fa6630b7
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2025-07-12 06:04:03 +0000

    add click for every single keys

--- Drum Kit Starting Files 2/index.js
+++ Drum Kit Starting Files 2/index.js
@@ -1,5 +1,7 @@
-document.querySelector("button").addEventListener(("click"), handleClick)
 
-function handleClick(){
-    console.log("I got clicked")
+var numberOfDrumsButton = document.querySelectorAll(".drum").length;
+for (var  i = 0; i < numberOfDrumsButton; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    alert("I got clicked");
+});
 }
