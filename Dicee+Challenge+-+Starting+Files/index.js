40d5362b0e5217fb322a283c6d62c8167b2cd3ed
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2025-07-12 04:06:47 +0000

    2

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -2,6 +2,6 @@ var randomNumber1 = (Math.floor((Math.random()*6))+1)
 
 var randomDiceImage = "dice" + randomNumber1 +".png"
 console.log(randomDiceImage)
-var randomImageSource = "./images/" + randomDiceImage
+var randomImageSource = "./images/dice6"
 console.log(randomImageSource)
 document.querySelectorAll("img")[0].setAttribute("src", "randomImageSource")
