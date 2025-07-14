75700862ef6ce90f84779a12018d73dd949c7b8d
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2025-07-14 09:17:56 +0000

    add images to buttons

--- Drum Kit Starting Files 2/index.js
+++ Drum Kit Starting Files 2/index.js
@@ -2,8 +2,8 @@
 var numberOfDrumsButton = document.querySelectorAll(".drum").length;
 for (var  i = 0; i < numberOfDrumsButton; i++){
 document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    var audio = new Audio('./sounds/crash.mp3')
-    audio.play()
+    var audio = new Audio('./sounds/crash.mp3');
+    audio.play();
     
 })
 }
