917e4ec2586834011443b52cea5a67102b7dc3d5
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2025-07-16 04:02:01 +0000

    creating native node module

---
+++ 2.2 Native Modules/index.js
@@ -0,0 +1,7 @@
+const { error } = require("console")
+const fs = require ("fs")
+
+fs.writeFile("Messege.txt", "Hello from Node JS", (err) => {
+if (err) throw err;
+console.log("The file has been saved!")
+});
