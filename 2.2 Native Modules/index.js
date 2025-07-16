c711444a34a46ad837dab393c35e6fef5f21a810
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2025-07-16 04:15:28 +0000

    console.log file in node the file that has been written

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,7 +1,12 @@
-const { error } = require("console")
-const fs = require ("fs")
+const { error } = require("console");
+const fs = require ("fs");
 
-fs.writeFile("Messege.txt", "Hello from Node JS", (err) => {
-if (err) throw err;
-console.log("The file has been saved!")
-});
+// fs.writeFile("Messege.txt", "Hello from Node JS", (err) => {
+// if (err) throw err;
+// console.log("The file has been saved!")
+// });
+
+fs.readFile("./Messege.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+}); 
